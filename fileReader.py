2b1175b087774e2e82bdea8d2fe9d979f897d3fb
Pedro Gonzaga <pgonzaga095@gmail.com>
2021-05-18 01:32:59 -0300

    mem management lru done

--- fileReader.py
+++ fileReader.py
@@ -42,3 +42,6 @@ class Reader():
             memoria = mm.Memory()
             nr_frames,numberList = memoria.getPages(fileName) # get number of frames availables and pages to be referenced
             memoria.fifo(nr_frames,numberList)
+
+
+            memoria.lru(nr_frames,numberList)
