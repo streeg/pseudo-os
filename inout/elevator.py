4c8583ae81c28874aa66e8bd8d512ee90843fa3a
robroger <robertacs42@gmail.com>
2021-05-19 01:36:22 -0300

    fcfs e ssf

--- inout/elevator.py
+++ inout/elevator.py
@@ -3,27 +3,28 @@
 class elevator:
 
     def processData(self, fileName):
+        count = 0
         fp = open(fileName, 'r')
         lines = fp.readlines()
-        maxCil = int(lines[0])
-        curCil = int(lines[1])
-        entries = []
-        count = 0
-        for line in lines[2:]:
-            entries.append(int(line))
-
-        # Direita para esquerda
-        maxRead = max(entries)
-
-        for i in range(curCil - 1, -1, -1):
-            count += 1
-            if i in entries:
-                entries.remove(i)
-
-        for i in range(1, maxRead + 1):
-            count += 1
-            if i in entries:
-                entries.remove(i)
+        if len(lines) > 2:
+            maxCil = int(lines[0])
+            curCil = int(lines[1])
+            entries = []
+            for line in lines[2:]:
+                entries.append(int(line))
+    
+            # Direita para esquerda
+            maxRead = max(entries)
+    
+            for i in range(curCil - 1, -1, -1):
+                count += 1
+                if i in entries:
+                    entries.remove(i)
+    
+            for i in range(1, maxRead + 1):
+                count += 1
+                if i in entries:
+                    entries.remove(i)
             
         fp.close()
         print('SCAN ', count)
--- inout/inoutModule.py
+++ inout/inoutModule.py
@@ -1,18 +1,18 @@
 import sys
 import numpy as np
-#from inout import firstComeFirstServe as fcfs
-#from inout import shortestSeekTimeFirst as sstf
+from inout import firstComeFirstServe as fcfs
+from inout import shortestSeekTimeFirst as sstf
 from inout import elevator as scan
 
 class Inout():
 
-    #def fcfs(self, fileName):
-    #    firstComeFirstServe = fcfs.firstComeFirstServe()
-    #    firstComeFirstServe.processData()
+    def fcfs(self, fileName):
+        firstComeFirstServe = fcfs.firstComeFirstServe()
+        firstComeFirstServe.processData(fileName)
 
-    #def sstf(self, fileName):
-    #    shortestSeekTimeFirst = sstf.shortestSeekTimeFirst()
-    #    firstComeFirstServe.processData()
+    def sstf(self, fileName):
+        shortestSeekTimeFirst = sstf.shortestSeekTimeFirst()
+        shortestSeekTimeFirst.processData(fileName)
         
     def scan(self, fileName):
         elevator = scan.elevator()
